25e51ea61e4b9c3caf4c765b075d21ad73a21e3b
Arunesh Singh <125174712+arunesh-singhh@users.noreply.github.com>
2023-07-20 19:14:51 +0530

    Add files via upload

---
+++ isosceles_pattern.java
@@ -0,0 +1,39 @@
+// input-4
+// output-
+//           1
+//          121
+//         12321
+//        1234321
+
+import java.util.Scanner;
+
+public class isosceles_pattern {
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int i = 1;
+
+        while(i<=n){
+            int space = 1;
+            while(space <= n - i){
+                System.out.print(" ");
+                space += 1;
+            }
+            int num = 1;
+            while(num <= i){
+                System.out.print(num);
+                num += 1;
+            }
+            int dec = i - 1;
+            while(dec >= 1){
+                System.out.print(dec);
+                dec -= 1;
+            }
+
+            System.out.println();
+            i += 1;
+        }
+        sc.close();
+    }
+}
